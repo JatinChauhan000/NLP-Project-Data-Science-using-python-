5fd7f0743b3814b88ffb66e26dd0068bfbc647df
JatinChauhan000 <108743660+JatinChauhan000@users.noreply.github.com>
2022-09-28 21:49:22 +0530

    Delete website (Python Project example) directory

--- website (Python Project example)/jatin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views # here . means all
-
-urlpatterns = [
-    path('',views.base, name='base'),
-    path('contact',views.contact, name='contact'),
-]
